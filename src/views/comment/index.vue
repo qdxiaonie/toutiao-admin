b091e50de76e2e073ea00a42de3e94ec02920a60
qdxiaonie <7842109+qdxn@user.noreply.gitee.com>
2020-12-02 12:50:18 +0800

    评论管理布局

---
+++ src/views/comment/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="comment-container">
+    <el-card class="filter-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>评论管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑导航 -->
+      </div>
+      <el-table class="comment-list" :data="articleData" style="width: 100%" stripe>
+        <el-table-column prop="title" label="标题"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">{{scope.row.comment_status ? '正常' : '关闭'}}</template>
+        </el-table-column>
+        <el-table-column prop="address" label="操作">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            ></el-switch>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+      ></el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { getArticles } from "@/api/article";
+export default {
+  created() {
+    this.loadArticles();
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+      articleData: [],
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4,
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+    loadArticles() {
+      getArticles({
+        response_type: "comment",
+      }).then((res) => {
+        this.articleData = res.data.data.results;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.comment-list {
+  margin-bottom: 30px;
+}
+</style>
