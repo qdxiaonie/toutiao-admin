7fd67725bb35a1342dc44390e0c4b7d7ef60d15b
qdxiaonie <7842109+qdxn@user.noreply.gitee.com>
2020-12-02 10:16:37 +0800

    图片删除及收藏

--- src/api/images.js
+++ src/api/images.js
@@ -7,11 +7,27 @@ export const uploadImage = data => {
         data
     })
 }
-
+// 获取图片素材
 export const getImages = params=> {
     return request({
         method:'GET',
         url: '/mp/v1_0/user/images',
        params
+    })
+}
+// 收藏图片素材
+export const collectImage =(imageId,collect)=> {
+    return request({
+        method:'PUT',
+        url: `/mp/v1_0/user/images/${imageId}`,
+       data:{collect}
+    })
+}
+// 删除图片素材
+export const deleteImage =(imageId)=> {
+    return request({
+        method:'DELETE',
+        url: `/mp/v1_0/user/images/${imageId}`,
+      
     })
 }
--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -10,7 +10,7 @@
         <!-- /面包屑导航 -->
       </div>
       <div class="btns">
-        <el-radio-group @change="onCollectChange" v-model="collect" size="mini">
+        <el-radio-group @change="loadImages(1)" v-model="collect" size="mini">
           <el-radio-button :label="false">全部</el-radio-button>
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
@@ -19,11 +19,52 @@
 
       <!-- 素材列表 -->
       <el-row :gutter="10">
-        <el-col :key="index" v-for="(img,index) in images" :xs="12" :sm="6" :md="6" :lg="4">
+        <el-col
+          class="img-box"
+          :key="index"
+          v-for="(img,index) in images"
+          :xs="12"
+          :sm="6"
+          :md="6"
+          :lg="4"
+        >
           <el-image style=" height: 100px" :src="img.url" fit="cover"></el-image>
+          <div class="image-action">
+            <el-button
+              type="warning"
+              :icon=" img.is_collected?'el-icon-star-on': 'el-icon-star-off'"
+              circle
+              size="mini"
+              :loading="img.loading"
+              @click="onCollect(img)"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete-solid"
+              circle
+              size="mini"
+              :loading="img.loading"
+              @click="onDelete(img)"
+            ></el-button>
+            <!-- <i
+              :class="{'el-icon-star-on':img.is_collected,'el-icon-star-off':!img.is_collected}"
+              @click="onCollect(img)"
+            ></i>-->
+            <!-- <i class="el-icon-delete-solid"></i> -->
+          </div>
         </el-col>
       </el-row>
       <!-- /素材列表 -->
+      <!-- 分页 -->
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :page-size="pageSize"
+        :total="totalCount"
+        :current-page.sync="page"
+        @current-change="onpageChange"
+      ></el-pagination>
+      <!-- /分页 -->
     </el-card>
     <el-dialog :append-to-body="true" title="上传素材" :visible.sync="dialogUploadVisible">
       <el-upload
@@ -33,7 +74,7 @@
         action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
         :headers="uploadHeader"
         :on-success="onUpLoadSuccess"
-        :show-file-list='false'
+        :show-file-list="false"
         multiple
       >
         <i class="el-icon-upload"></i>
@@ -48,7 +89,7 @@
 </template>
 
 <script>
-import { getImages } from "@/api/images";
+import { getImages, collectImage ,deleteImage} from "@/api/images";
 export default {
   data() {
     const user = JSON.parse(window.localStorage.getItem("user"));
@@ -59,26 +100,55 @@ export default {
       uploadHeader: {
         Authorization: `Bearer ${user.token}`,
       },
+      pageSize: 10,
+      page: 1,
+      totalCount: 0,
     };
   },
   created() {
-    this.loadImages(false);
+    this.loadImages(1);
   },
   methods: {
-    loadImages(collect) {
+    loadImages(page) {
+      this.page = page;
       getImages({
-        collect,
+        collect: this.collect,
+        page,
+        per_page: this.pageSize,
       }).then((res) => {
-        this.images = res.data.data.results;
+        const results = res.data.data.results;
+        results.forEach((img) => {
+          img.loading = false;
+        });
+        this.images = results;
+        this.totalCount = res.data.data.total_count;
       });
-    },
-    onCollectChange(value) {
-      this.loadImages(value);
     },
     onUpLoadSuccess() {
       this.dialogUploadVisible = false;
-      this.loadImages(false);
+      this.loadImages(this.page);
+      this.$message({
+        type: "success",
+        message: "上传成功",
+      });
     },
+    onpageChange(page) {
+      this.loadImages(page);
+    },
+    onCollect(img) {
+      img.loading = true;
+      collectImage(img.id, !img.is_collected).then((res) => {
+        img.is_collected = !img.is_collected;
+        img.loading = false;
+      });
+    },
+    onDelete(img){
+      img.loading = true;
+      deleteImage(img.id).then((res) => {
+        this.loadImages();
+        img.loading = false;
+      })
+    }
   },
 };
 </script>
@@ -88,5 +158,20 @@ export default {
   display: flex;
   justify-content: space-between;
   padding-bottom: 20px;
+}
+.img-box {
+  position: relative;
+}
+.image-action {
+  position: absolute;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  bottom: 4px;
+  font-size: 20px;
+  text-align: center;
+  background-color: rgba(248, 247, 247, 0.5);
 }
 </style>
