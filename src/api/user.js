9e7b724faafb894a4f6e64efb479d61d6ce36ddb
qdxiaonie <7842109+qdxn@user.noreply.gitee.com>
2020-12-06 16:41:51 +0800

    修改头像

--- src/api/user.js
+++ src/api/user.js
@@ -18,3 +18,21 @@ export const getUserProfile = () => {
         //  } 
     })
 }
+//修改用户资料
+export const updateUserProfile = data => {
+   
+    return request({
+        method: 'PATCH',
+        url: '/mp/v1_0/user/profile',
+        data
+    })
+}
+//修改用户头像
+export const updateUserPhoto = data => {
+   
+    return request({
+        method: 'PATCH',
+        url: '/mp/v1_0/user/photo',
+        data
+    })
+}
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -11,55 +11,97 @@
       </div>
       <el-row>
         <el-col :span="14">
-          <el-form ref="form" :model="user" label-width="80px">
+          <el-form ref="update-form" :rules="formRules" :model="user" label-width="80px">
             <el-form-item label="编号:">{{user.id}}</el-form-item>
             <el-form-item label="手机:">{{user.mobile}}</el-form-item>
-            <el-form-item label="媒体名称">
+            <el-form-item prop="name" label="媒体名称">
               <el-input v-model="user.name"></el-input>
             </el-form-item>
-            <el-form-item label="媒体介绍">
+            <el-form-item prop="intro" label="媒体介绍">
               <el-input type="textarea" v-model="user.intro"></el-input>
             </el-form-item>
-            <el-form-item label="邮箱">
+            <el-form-item prop="email" label="邮箱">
               <el-input v-model="user.email"></el-input>
             </el-form-item>
 
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">保存</el-button>
+              <el-button :loading="loading" type="primary" @click="updateUser">保存</el-button>
             </el-form-item>
           </el-form>
         </el-col>
         <el-col :offset="2" :span="8">
-          <el-avatar shape="square" :size="160" fit="cover" :src="user.photo"></el-avatar>
-          <p>点击修改图像</p>
+          <label for="file">
+            <el-avatar shape="square" :size="160" fit="cover" :src="user.photo"></el-avatar>
+            <!-- <p @click="$refs.file.click()">点击修改图像</p>
+            -->
+            <p>点击修改图像</p>
+          </label>
+          <input @change="onFileChange" type="file" id="file" ref="file" hidden />
         </el-col>
+
+        <el-dialog
+          append-to-body
+          title="提示"
+          :visible.sync="dialogVisible"
+          width="30%"
+          :before-close="handleClose"
+          @opened="onDialogOpened"
+        >
+          <div>
+            <img class="preview-image" ref="preview-image" width="200px" :src="previewImage" alt />
+          </div>
+
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button :loading="updatePhotoLoading" type="primary" @click="updatePhoto">确 定</el-button>
+          </span>
+        </el-dialog>
       </el-row>
     </el-card>
   </div>
 </template>
 
 <script>
-import { getUserProfile } from "@/api/user";
+import { getUserProfile, updateUserProfile, updateUserPhoto } from "@/api/user";
+import "cropperjs/dist/cropper.css";
+import Cropper from "cropperjs";
 export default {
   data() {
     return {
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-      },
+      loading: false,
+      dialogVisible: false,
+      previewImage: "",
+      cropper: null,
+      updatePhotoLoading: false,
       user: {
-          email:'',
-          id:null,
-          intro:'',
-          mobile:'',
-          name:'',
-          photo:''
+        email: "",
+        id: null,
+        intro: "",
+        mobile: "",
+        name: "",
+        photo: "",
+      },
+      formRules: {
+        name: [
+          { required: true, message: "请输入文章标题", trigger: "blur" },
+          { min: 1, max: 7, message: "长度在1到7个字符之间", trigger: "blur" },
+        ],
+        intro: [
+          { required: true, message: "请输入文章标题", trigger: "blur" },
+          {
+            min: 0,
+            max: 60,
+            message: "长度在0到60个字符之间",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          {
+            pattern: /^([a-zA-Z\d])(\w|\-)+@[a-zA-Z\d]+\.[a-zA-Z]{2,4}$/,
+            message: "请输入正确的邮箱格式",
+            trigger: "blur",
+          },
+        ],
       },
     };
   },
@@ -67,13 +109,72 @@ export default {
     this.loadUser();
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    updateUser() {
+      this.loading = true;
+      this.$refs["update-form"].validate((valid) => {
+        if (valid) {
+          updateUserProfile(this.user).then((res) => {
+            this.$message({
+              type: "success",
+              message: "更新成功",
+            });
+            this.loading = false;
+          });
+        } else {
+          return false;
+        }
+      });
     },
     loadUser() {
       getUserProfile().then((res) => {
-        console.log(res);
+        // console.log(res);
         this.user = res.data.data;
+      });
+    },
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {});
+    },
+
+    onFileChange() {
+      // 处理图片预览
+      const file = this.$refs.file;
+      // console.log(file.files[0]);
+      const blobData = window.URL.createObjectURL(file.files[0]);
+      this.previewImage = blobData;
+
+      this.dialogVisible = true;
+
+      // console.log("change");
+      // 解决选择相同文件时不触发点击事件
+      this.$refs.file.value = "";
+    },
+    onDialogOpened() {
+      if (this.cropper) {
+        this.cropper.replace(this.previewImage);
+        return;
+      }
+      const image = this.$refs["preview-image"];
+      this.cropper = new Cropper(image, {
+        aspectRatio: 1,
+        viewMode: 1,
+        dragMode: "none",
+      });
+    },
+    updatePhoto() {
+      this.updatePhotoLoading = true;
+      this.cropper.getCroppedCanvas().toBlob((file) => {
+        // console.log(file);
+        const fd = new FormData();
+        fd.append("photo", file);
+        updateUserPhoto(fd).then((res) => {
+          this.user.photo = window.URL.createObjectURL(file);
+          this.dialogVisible = false;
+          this.updatePhotoLoading = false;
+        });
       });
     },
   },
@@ -81,4 +182,9 @@ export default {
 </script>
 
 <style>
+.preview-image {
+  display: block;
+  max-width: 100%;
+  height: 200px;
+}
 </style>
